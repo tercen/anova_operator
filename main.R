e56becca3a0009479bbe04af954a26cd102ad122
agouy <contact@agouy.com>
2020-12-11 22:16:35 +0000

    added more stats

--- main.R
+++ main.R
@@ -2,16 +2,35 @@ library(tercen)
 library(dplyr)
 
 do.anova = function(df){
-  pr = NaN
+  f.stat = NaN
+  numdf = NaN
+  dendf = NaN
+  p.value = NaN
+  r.squared = NaN
+  adj.r.squared = NaN
   aLm = try(lm(.y ~ .group.colors, data=df), silent = TRUE)
-  if(!inherits(aLm, 'try-error')) pr = (anova(aLm)$'Pr(>F)')[[1]]
-  return (data.frame(.ri = df$.ri[1], .ci = df$.ci[1], pr= c(pr)))
+  if(!inherits(aLm, 'try-error')) {
+    p.value = (anova(aLm)$'Pr(>F)')[[1]]
+    sm <- summary(aLm)
+    f.stat <- sm$fstatistic[1]
+    numdf <- sm$fstatistic[2]
+    dendf <- sm$fstatistic[3]
+    r.squared <- sm$r.squared
+    adj.r.squared <- sm$adj.r.squared
+  } 
+  return (data.frame(.ri = df$.ri[1], .ci = df$.ci[1],
+                     f.stat = c(f.stat),
+                     numdf = c(numdf),
+                     dendf = c(dendf),
+                     p.value = c(p.value),
+                     r.squared = c(r.squared),
+                     adj.r.squared = c(adj.r.squared)))
 }
-  
+
 ctx = tercenCtx()
 
 if (length(ctx$colors) < 1) stop("A color factor is required.")
- 
+
 ctx %>% 
   select(.ci, .ri, .y) %>%
   mutate(.group.colors = do.call(function(...) paste(..., sep='.'), ctx$select(ctx$colors))) %>%
