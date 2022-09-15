4caa229d8e85075ccbb86746e9120307b0318dda
agouy <contact@agouy.com>
2022-09-15 08:12:04 +0000

    full refactoring

--- main.R
+++ main.R
@@ -1,40 +1,36 @@
-library(tercen)
-library(dplyr)
-
-do.anova = function(df){
-  f.stat = NaN
-  numdf = NaN
-  dendf = NaN
-  p.value = NaN
-  r.squared = NaN
-  adj.r.squared = NaN
-  aLm = try(lm(.y ~ .group.colors, data=df), silent = TRUE)
-  if(!inherits(aLm, 'try-error')) {
-    p.value = (anova(aLm)$'Pr(>F)')[[1]]
-    sm <- summary(aLm)
-    f.stat <- sm$fstatistic[1]
-    numdf <- sm$fstatistic[2]
-    dendf <- sm$fstatistic[3]
-    r.squared <- sm$r.squared
-    adj.r.squared <- sm$adj.r.squared
-  } 
-  return (data.frame(.ri = df$.ri[1], .ci = df$.ci[1],
-                     f.stat = c(f.stat),
-                     numdf = c(numdf),
-                     dendf = c(dendf),
-                     p.value = c(p.value),
-                     r.squared = c(r.squared),
-                     adj.r.squared = c(adj.r.squared)))
-}
+suppressPackageStartupMessages({
+  library(tercen)
+  library(tercenApi)
+  library(dplyr)
+  library(rstatix)
+})
 
 ctx = tercenCtx()
 
-if (length(ctx$colors) < 1) stop("A color factor is required.")
+if(length(ctx$colors) < 1) stop("A color factor is required.")
 
-ctx %>% 
-  select(.ci, .ri, .y) %>%
-  mutate(.group.colors = do.call(function(...) paste(..., sep='.'), ctx$select(ctx$colors))) %>%
+method <- ctx$op.value('method', as.character, 'one.way')
+collapse_str <- c("one.way" = " + ", "two.way" = " * ")[method]
+
+col_names <- unlist(ctx$colors)
+fixed_effect <- paste0(
+  paste0("`", col_names, "`"),
+  collapse = collapse_str
+)
+
+## If labels, add random effect:
+lab_names <- unlist(ctx$labels)
+if(!is.null(lab_names)) {
+  random_effect <- paste0(" + Error(", paste0("`", lab_names, "`"), "/(", fixed_effect, "))")
+  form <- formula(paste0(paste0(".y ~ ", fixed_effect), random_effect))
+} else {
+  form <- formula(paste0(".y ~ ", fixed_effect))
+}
+
+df <- ctx$select(c(".ci", ".ri", ".y", col_names, lab_names)) %>%
   group_by(.ci, .ri) %>%
-  do(do.anova(.)) %>%
+  anova_test(., form) %>%
+  as_tibble() %>%
+  rename(significance = !!sym("p<.05")) %>%
   ctx$addNamespace() %>%
   ctx$save()
